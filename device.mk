mizdrake7/android_device_xiaomi_air

air: Configure Virtual A/B OTAs * Enable recovery-as-boot for A/B device. * Build updater packages for A/B device. * Include checkpoint_gc & otapreopt_script postinstall   scripts. * Configure partitions which will be included in OTA package. * Inherit the compression.mk product configurations as   seen on stock.
Co-authored-by: Mashopy <eliasgheeraert@gmail.com>
Change-Id: I1d89af2a97450023332f054be461caf85dc819f1

device.mk
@@ -12,3 +12,29 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Inherit virtual_ab_ota product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# A/B
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    otapreopt_script \
+    checkpoint_gc
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_PARTITION_TYPE) \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/mtk_plpath_utils_ota \
+    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
+    POSTINSTALL_OPTIONAL_vendor=true

Koushikdey2003/device_xiaomi_air-recovery

device.mk
@@ -12,3 +12,29 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Inherit virtual_ab_ota product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# A/B
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    otapreopt_script \
+    checkpoint_gc
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_PARTITION_TYPE) \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
+    POSTINSTALL_OPTIONAL_vendor=true