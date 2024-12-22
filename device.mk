283acb421ca18cbac17151c8a92be08bc57c117d
Koushik Dey <kousickdey3@gmail.com>
2024-12-22 22:30:42 +0530

    air: Switch to AIDL Health service
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>
    Change-Id: I7efd3ec8766a9c132950a53d65bdf7a7e3957a3a

--- device.mk
+++ device.mk
@@ -58,6 +58,5 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.example-defaults \
+    android.hardware.health-service.example_recovery
