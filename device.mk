d06d04c6db18ff878a082b4b278377d907712cc9
Koushik Dey <kousickdey3@gmail.com>
2025-01-19 18:37:24 +0530

    air: Configure A/B OTAs
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>
    Change-Id: Ia29950c20703cd5684f7cefffc9575d3f5b49e91

--- device.mk
+++ device.mk
@@ -4,6 +4,41 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# A/B OTA Configuration
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    vendor_boot \
+    system \
+    vendor \
+    product \
+    odm \
+    vendor_dlkm \
+    odm_dlkm \
+    system_dlkm \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    checkpoint_gc
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
