996cdfc5603fc643ca06d370f76bbe965fcfc147
CaptainThrowback <captainthrowback@hotmail.com>
2026-02-10 01:20:49 +0530

    air: Set TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI to skip GKI folders
    * it exclude GKI folder parsing from load modules (log spam fix)
    
    Change-Id: I25e0da2eb395bdca3cc59d4664823429254f940c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 # Load vendor_boot modules
 TW_LOAD_VENDOR_BOOT_MODULES := true
 TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\")
+TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
